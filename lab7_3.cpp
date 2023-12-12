a0a035f1d682ff0a8d1d7c416a9c0fc371ddc0b3
DmeDream <thanaboon1357@gmail.com>
2023-12-12 14:39:58 +0700

    complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,18 +2,13 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+	if( x >= 66 && x <= 90)
+	{
+		return x-1;
+	}
+	else if ( x == 65)
+	{
+		return 'Z';
+	}
+	else return '0';
 }
